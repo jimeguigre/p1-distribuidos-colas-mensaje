2db81cb27e40e7d8ff062b84abdd37448d2101d9
jimeguigre <jimeguigre@gmail.com>
2026-03-15 17:54:11 +0100

    prueba con tiempos

--- pruebas/app_cliente_test_carga.c
+++ pruebas/app_cliente_test_carga.c
@@ -5,12 +5,19 @@
 #include <unistd.h>
 #include "../claves.h"
 
+#include <sys/time.h>
+
+struct timeval start, end;
+double tiempo_total;
+
+
 int main() {
     char key[256];
     float v2[1] = {5.5};
     struct Paquete p = {0,0,0};
     int n2;
     char v1_res[256];
+    gettimeofday(&start, NULL); // Marca de tiempo inicial
 
     for (int i = 0; i < 50; i++) {
         sprintf(key, "multi_%d_%d", getpid(), i);
@@ -24,6 +31,17 @@ int main() {
             exit(-1);
         }
     }
+    gettimeofday(&end, NULL);   // Marca de tiempo final
+
+    // Calcular tiempo en segundos
+    tiempo_total = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+
+    printf("\n--- ESTADÍSTICAS DE RENDIMIENTO ---\n");
+    printf("Tiempo total: %.4f segundos\n", tiempo_total);
+    printf("Operaciones por segundo (Throughput): %.2f ops/sec\n", 500 / tiempo_total);
+    printf("Latencia media: %.4f ms/op\n", (tiempo_total / 500) * 1000);
     printf("Cliente %d: 50 ciclos de Set/Get completados con éxito.\n", getpid());
     return 0;
+
 }
+
--- pruebas/test_carga.sh
+++ pruebas/test_carga.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# 1. Limpieza de colas y procesos antiguos
+# Limpieza de colas y procesos antiguos
 rm /dev/mqueue/* 2>/dev/null
 killall servidor_mq 2>/dev/null
 
-# 2. Compilar (por si acaso hubo cambios)
-make
-
-# 3. Iniciar el servidor
+# Iniciar el servidor
 ./servidor_mq &
 SERV_PID=$!
 sleep 1
 
 echo "--- INICIANDO PRUEBA DE CARGA (500 OPERACIONES) ---"
 
-# 4. Lanzar 10 clientes en paralelo
-for i in {1..10}
+# Lanzar 10 clientes en paralelo
+for i in {1..100}
 do
-   ./app_cliente &
+   ./app_cliente_test_carga &
 done
 
 # 5. Esperar a que todos los hilos terminen
 wait
 
 echo "--- PRUEBA FINALIZADA ---"
-
-# Opcional: Matar el servidor al terminar
-kill $SERV_PID 2>/dev/null
